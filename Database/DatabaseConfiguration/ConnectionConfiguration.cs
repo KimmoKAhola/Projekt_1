36ed9491f390dfd906ed569dd1592d6995a2a45d
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-09 12:26:56 +0100

    added appsettings.json as well as a config method for the connection settings.

---
+++ Database/DatabaseConfiguration/ConnectionConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.DatabaseConfiguration
+{
+    public static class ConnectionConfiguration
+    {
+        public static DbContextOptionsBuilder<DatabaseContext> Build()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            var connectionString = GetConnectionString();
+
+            var options = new DbContextOptionsBuilder<DatabaseContext>();
+
+            options.UseSqlServer(connectionString);
+
+            return options;
+        }
+
+        public static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            return connectionString;
+        }
+    }
+}
--- Database/DatabaseConfiguration/DatabaseContext.cs
+++ Database/DatabaseConfiguration/DatabaseContext.cs
@@ -22,15 +22,13 @@ namespace Database.DatabaseConfiguration
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+
             optionsBuilder.UseSqlServer("Server=localhost;Database=Projekt_1;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Result>()
-            //    .HasMany(r => r.AreaCalculations)
-            //    .WithOne(ac => ac.Result)
-            //    .HasForeignKey(ac => ac.Result);
+
         }
     }
 }
