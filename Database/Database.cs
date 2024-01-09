ff91c7c5e9c199766bdce99ece6ac5da31ec3ce3
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-09 10:02:55 +0100

    added the database configuration class

---
+++ Database/Database.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+
+namespace Database
+{
+    public class Database : DbContext
+    {
+        public Database()
+        {
+
+        }
+
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=TheSuiteSpot;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true");
+        }
+    }
+}
