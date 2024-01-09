c4730c0fa887f94a3092eb9adada128b2b81f38a
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-09 10:39:41 +0100

    Added the four math operations: addition, subtraction, multiplication and division.

--- Calculator/MathOperations.cs
+++ Calculator/MathOperations.cs
@@ -2,6 +2,24 @@
 {
     public class MathOperations
     {
+        public static decimal Addition(decimal first, decimal second)
+        {
+            return first + second;
+        }
 
+        public static decimal Subtraction(decimal first, decimal second)
+        {
+            return first - second;
+        }
+
+        public static decimal Multiplication(decimal first, decimal second)
+        {
+            return first * second;
+        }
+
+        public static decimal Division(decimal first, decimal second)
+        {
+            return first / second;
+        }
     }
 }
