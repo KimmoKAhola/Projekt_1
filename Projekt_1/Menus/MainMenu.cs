13053abc9fee924294da4fedcc2cf63e07fb5518
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-10 18:03:53 +0100

    I added my three game menus to my main menu. I now have a working game loop from program.cs

--- Projekt_1/Menus/MainMenu.cs
+++ Projekt_1/Menus/MainMenu.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Calculator.Mathematics;
+using Database.Models;
+using Database.Services;
+using InputValidationLibrary;
+using Projekt_1.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,51 @@ using System.Threading.Tasks;
 
 namespace Projekt_1.Menus
 {
-    public class MainMenu
+    public class MainMenu(MathContext context, DatabaseService service, MathCalculation calculation) : IMenu
     {
+        private List<IMenu> _menus = new List<IMenu>()
+        {
+            new CalculatorMenu(context, service, calculation),
+            new AreaCalculatorMenu(context, service),
+            new RockPaperScissorsMenu(),
+        };
+        public void Display()
+        {
+            PrintBanner();
+        }
 
+
+        public void Menuchoice(int choice)
+        {
+            _menus[choice - 1].Run();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Display();
+                var choice = PromptUser();
+                Menuchoice(choice);
+            }
+        }
+        public void PrintBanner()
+        {
+            string banner = @"****************************************************************************
+*███╗   ███╗ █████╗ ██╗███╗   ██╗    ███╗   ███╗███████╗███╗   ██╗██╗   ██╗*
+*████╗ ████║██╔══██╗██║████╗  ██║    ████╗ ████║██╔════╝████╗  ██║██║   ██║*
+*██╔████╔██║███████║██║██╔██╗ ██║    ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║*
+*██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║*
+*██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝*
+*╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ *
+****************************************************************************";
+            Console.WriteLine(banner);
+        }
+
+        public int PromptUser()
+        {
+            return UserInputValidation.MenuValidation(_menus, "Choose which game to start: ");
+        }
     }
 }
