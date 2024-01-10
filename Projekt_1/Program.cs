68201e67b9927a412bd87c62237cb715798b8718
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-10 18:04:16 +0100

    cleaned the program.cs class.

--- Projekt_1/Program.cs
+++ Projekt_1/Program.cs
@@ -31,52 +31,43 @@ namespace Projekt_1
             var config = Configuration.Configure();
             using (var scope = config.BeginLifetimeScope())
             {
-                var mathContext = scope.Resolve<MathContext>();
-                var areaContext = scope.Resolve<AreaCalculatorContext>();
-                var databaseService = scope.Resolve<DatabaseService>();
-                var menuService = scope.Resolve<CalculatorMenu>();
-
-                if (UserInputValidation.PromptYesOrNo("Press y to do math, n to do area: "))
-                {
-                    using (var ctx = new DatabaseContext())
-                    {
-                        while (true)
-                        {
-                            Console.Write("Enter a number 1-6: ");
-                            var choice = Convert.ToInt32(Console.ReadLine());
-                            IMathStrategy? math = mathContext.SetStrategy(choice);
-
-                            var mathResult = mathContext.Calculate(15, 35);
-
-                            var calculation = new MathCalculation
-                            {
-                                FirstInput = 15,
-                                SecondInput = 35,
-                                Answer = mathResult,
-                                Operator = math.Operator,
-                                Result = new Result
-                                {
-                                    DateCreated = DateTime.Now,
-                                    ResultType = ResultTypes.MathCalculation.ToString(),
-                                }
-                            };
-
-                            databaseService.AddCalculation(calculation);
-                            Console.WriteLine($"The result is: {mathResult}");
-                        }
-                    }
-                }
-                else
-                {
-                    using (var ctx = new DatabaseContext())
-                    {
-                        while (true)
-                        {
-                            menuService.Display();
-                        }
-                    }
-                }
+                //var mathContext = scope.Resolve<MathContext>();
+                //var areaContext = scope.Resolve<AreaCalculatorContext>();
+                //var databaseService = scope.Resolve<DatabaseService>();
+                var menuService = scope.Resolve<MainMenu>();
+                menuService.Run();
             }
         }
     }
 }
+
+//if (UserInputValidation.PromptYesOrNo("Press y to do math, n to do area: "))
+//{
+//    using (var ctx = new DatabaseContext())
+//    {
+//        while (true)
+//        {
+//            Console.Write("Enter a number 1-6: ");
+//            var choice = Convert.ToInt32(Console.ReadLine());
+//            IMathStrategy? math = mathContext.SetStrategy(choice);
+
+//            var mathResult = mathContext.Calculate(15, 35);
+
+//            var calculation = new MathCalculation
+//            {
+//                FirstInput = 15,
+//                SecondInput = 35,
+//                Answer = mathResult,
+//                Operator = math.Operator,
+//                Result = new Result
+//                {
+//                    DateCreated = DateTime.Now,
+//                    ResultType = ResultTypes.MathCalculation.ToString(),
+//                }
+//            };
+
+//            databaseService.AddCalculation(calculation);
+//            Console.WriteLine($"The result is: {mathResult}");
+//        }
+//    }
+//}
