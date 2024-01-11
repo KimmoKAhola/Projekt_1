acb1d4a7800208c3c70fc1219d0f4df325fe795a
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-11 21:03:55 +0100

    Added a gamelogic class to my RPS class.

---
+++ Rock_Paper_Scissors/GameLogic.cs
@@ -0,0 +1,26 @@
+﻿namespace Rock_Paper_Scissors
+{
+    public static class GameLogic
+    {
+        public static string CheckForWin(string userInput, string computerInput)
+        {
+            if (userInput == computerInput)
+            {
+                Console.WriteLine($"{GameState.Tie}");
+                return GameState.Tie.ToString();
+            }
+            else if (userInput == Move.Rock.ToString() && computerInput == Move.Scissors.ToString()
+                || userInput == Move.Scissors.ToString() && computerInput == Move.Paper.ToString()
+                || userInput == Move.Paper.ToString() && computerInput == Move.Rock.ToString())
+            {
+                Console.WriteLine($"{GameState.Win}");
+                return GameState.Win.ToString();
+            }
+            else
+            {
+                Console.WriteLine($"{GameState.Loss}");
+                return GameState.Loss.ToString();
+            }
+        }
+    }
+}
