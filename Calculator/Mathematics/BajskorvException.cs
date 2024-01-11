e36f1165b0e5a0dffde5d3683d5cd0c57677c123
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-11 19:51:36 +0100

    Removed a warning from this temporary class

---
+++ Calculator/Mathematics/BajskorvException.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Calculator.Mathematics
+{
+    internal class BajskorvException : Exception
+    {
+        public BajskorvException()
+        {
+        }
+
+        public BajskorvException(string? message) : base(message)
+        {
+        }
+
+        public BajskorvException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
