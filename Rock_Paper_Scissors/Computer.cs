e4a771c62f5bf6008ff53361b1ddb21e7bc66389
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-11 21:04:10 +0100

    Added a method to get the computer move.

---
+++ Rock_Paper_Scissors/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    public static class Computer
+    {
+        public static Move GetComputerMove()
+        {
+            var random = new Random();
+            int chosenMove = random.Next(0, 3);
+
+            return (Move)chosenMove;
+        }
+    }
+}
