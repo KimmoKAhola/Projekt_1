2604e74eb9d1d7ee979be9ca737b6840b1b822cf
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-11 19:53:42 +0100

    Added names to the shape classes. Also changed the calculate method to a tuple instead of having two methods.

--- Calculator/Shapes/Rectangle.cs
+++ Calculator/Shapes/Rectangle.cs
@@ -11,16 +11,11 @@ namespace Calculator.Shapes
     {
         public double Width { get; set; }
         public double Height { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "Rectangle";
 
-        public double CalculateArea(double width, double height)
+        public (double area, double circumference) Calculate(double width, double height)
         {
-            return width * height;
-        }
-
-        public double CalculateCircumference(double width, double height)
-        {
-            return width * 2 + height * 2;
+            return (width * height, (width * 2 + height * 2));
         }
     }
 }
--- Calculator/Shapes/Triangle.cs
+++ Calculator/Shapes/Triangle.cs
@@ -11,16 +11,11 @@ namespace Calculator.Shapes
     {
         public double Width { get; set; }
         public double Height { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "Triangle";
 
-        public double CalculateArea(double width, double height)
+        public (double area, double circumference) Calculate(double width, double height)
         {
-            return (width * height) / 2;
-        }
-
-        public double CalculateCircumference(double width, double height)
-        {
-            return 3 * width; //Asume equilaterial triangle
+            return ((width * height) / 2, width * 3);
         }
     }
 }
