aae04e2e6b7b6794233621d23cbf010051e47823
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-15 14:33:47 +0100

    I have copied the update method from Area calculation and changed the inputs to relevant inputs for math calculation. Also added some extra logic - when changing a propery the answer has to be calculated again

--- Database/Services/MathCalculationService.cs
+++ Database/Services/MathCalculationService.cs
@@ -57,12 +57,97 @@ namespace Database.Services
 
         public void UpdateCalculation(int id)
         {
-            var calculation = _calculationRepository.Get(id);
-            _calculationRepository.Update(calculation);
-            _calculationRepository.Save();
-            PrintMessages.PrintNotification($"{calculation} has been updated.");
+            MathCalculation? entityToUpdate = _calculationRepository.Get(id);
+            int? chosenProperty = PromptUpdate();
+            if (entityToUpdate != null)
+            {
+                ChangeOption(chosenProperty, entityToUpdate);
+                _context.SetStrategy(entityToUpdate.Operator);
+                entityToUpdate.Answer = _context.ExecuteStrategy(entityToUpdate.FirstInput, entityToUpdate.SecondInput);
+                if (!double.IsNaN(entityToUpdate.Answer))
+                {
+                    _calculationRepository.Update(entityToUpdate);
+                    _calculationRepository.Save();
+                }
+                else
+                {
+                    PrintMessages.PrintErrorMessage("Invalid input for that operator.");
+                }
+            }
+            else
+            {
+                PrintMessages.PrintErrorMessage("No entity with that id was found.");
+            }
         }
 
+        private void ChangeOption(int? choice, MathCalculation entity)
+        {
+            switch (choice - 1)
+            {
+                case 0:
+                    UpdateFirstInput(entity);
+                    break;
+                case 1:
+                    UpdateSecondInput(entity);
+                    break;
+                case 2:
+                    UpdateOperator(entity);
+                    break;
+                case 3:
+                    DeleteCalculation(entity.Id);
+                    break;
+            }
+        }
+        private static void UpdateFirstInput(MathCalculation entity)
+        {
+            Console.Write("Enter the new input: ");
+            if (double.TryParse(Console.ReadLine(), out double result))
+            {
+                entity.FirstInput = result;
+            }
+            else
+            {
+                PrintMessages.PrintErrorMessage("Incorrect input.");
+            }
+        }
+        private static void UpdateSecondInput(MathCalculation entity)
+        {
+            Console.Write("Enter the new input: ");
+            if (double.TryParse(Console.ReadLine(), out double result))
+            {
+                entity.SecondInput = result;
+            }
+            else
+            {
+                PrintMessages.PrintErrorMessage("Incorrect input.");
+            }
+        }
+
+
+        private static void UpdateOperator(MathCalculation entity)
+        {
+            Console.Clear();
+            var listOfOperators = GetMathOperators();
+            int? choice = UserInputValidation.MenuValidation(listOfOperators, "HEJ HOPP");
+            if (choice != null)
+            {
+                var op = listOfOperators[(int)choice - 1];
+                entity.Operator = op;
+            }
+        }
+
+
+        private static int? PromptUpdate()
+        {
+            Dictionary<int, string> options = new()
+            {
+                {1, "First input" },
+                {2, "Second input" },
+                {3, "Operator input" },
+                {4, "Delete" },
+            };
+            return UserInputValidation.MenuValidation(options, "TEMPORARY");
+        }
         public static ICalculation? CreateMathCalculation(MathContext mathStrategy)
         {
             double[]? numbers = UserInputValidation.ReturnTwoNumbersForMath("Enter two numbers: ");
