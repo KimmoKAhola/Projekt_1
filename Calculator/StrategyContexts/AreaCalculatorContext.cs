277190885c3bfbe7a661312c2576bdbb7ec3cd30
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-15 10:43:39 +0100

    Moved my strategy contexts to a separate folder.

---
+++ Calculator/StrategyContexts/AreaCalculatorContext.cs
@@ -0,0 +1,42 @@
+﻿using Calculator.Interfaces;
+using Calculator.Mathematics.Operations;
+using Calculator.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations.StrategyContexts
+{
+    public class AreaCalculatorContext
+    {
+        public IShape? Strategy { get; private set; } = null;
+        public string ShapeName { get; private set; } = string.Empty;
+        private static IShape? CreateStrategy(string? choice)
+        {
+            return choice?.ToLower() switch //'▬', '▲', 'P', '♦'
+            {
+                "rectangle" => new Rectangle(),
+                "parallelogram" => new Parallelogram(),
+                "triangle" => new Triangle(),
+                "rhomboid" => new Rhomboid(),
+                null => null,
+                _ => null,
+            };
+        }
+
+        public void SetStrategy(string choice)
+        {
+            Strategy = CreateStrategy(choice);
+            if (Strategy != null)
+                ShapeName = Strategy.Name;
+        }
+
+        public (double area, double circumference) ExecuteStrategy(double width, double height)
+        {
+            var (area, circumference) = Strategy.Calculate(width, height);
+            return (Math.Round(area, 2), Math.Round(circumference, 2));
+        }
+    }
+}
