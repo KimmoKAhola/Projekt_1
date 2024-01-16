9f4b46e2eb6b24403465ac99b6976097aa5de32f
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-16 14:31:47 +0100

    I created a repository for my HighScore table with relevant methods. Since this is not CRUD I have not implemented the IRepository interface.

---
+++ Database/Repositories/HighScoreRepository.cs
@@ -0,0 +1,31 @@
+﻿using Database.DatabaseConfiguration;
+using Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Rock_Paper_Scissors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Repositories
+{
+    public class HighScoreRepository(DatabaseContext dbContext)
+    {
+        private readonly DatabaseContext _dbContext = dbContext;
+        public void Add(HighScore entity)
+        {
+            _dbContext.Add(entity);
+        }
+
+        public HighScore? Get()
+        {
+            return _dbContext.HighScore.FirstOrDefault();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
