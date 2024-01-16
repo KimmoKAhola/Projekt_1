e738226f4c4b0994bada5945895fb2471be6f938
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2024-01-16 16:17:04 +0100

    Removed the parameters from my Ishape interface. Looks cleaner now and performs the same.

--- Calculator/Interfaces/IShape.cs
+++ Calculator/Interfaces/IShape.cs
@@ -5,6 +5,6 @@
         public double Width { get; set; }
         public double Height { get; set; }
         public string Name { get; set; }
-        public (double area, double circumference) Calculate(double width, double height);
+        public (double area, double circumference) Calculate();
     }
 }
